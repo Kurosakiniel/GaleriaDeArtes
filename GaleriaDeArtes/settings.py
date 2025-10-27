a2f2c2bd0608bb901a830900406dbb499ade7a57
Kurosakiniel <ichigoniel@gmail.com>
2025-10-27 17:11:58 -0300

    Apagar depois

--- GaleriaDeArtes/settings.py
+++ GaleriaDeArtes/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'GaleriaDeArtes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SegundoEstagio',
+        'USER': 'postgres',
+        'PASSWORD': 'RaparigaJohto',
+        'Host': 'localhost'
     }
 }
 
@@ -116,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # pasta global do projeto
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
